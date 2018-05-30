1f990984c90fa603f777574db16cb91978a4f20d
Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
2018-05-30 17:30:29 +0200

    Emacs 26.1

--- emacs/customizations/02-global.el
+++ emacs/customizations/02-global.el
@@ -3,7 +3,6 @@
 ;;; Code:
 
 ;;; Fancier dired display
-(require 'dired-details+)
 (ido-vertical-mode)
 (provide '02-global)
 ;;; 02-global.el ends here
--- emacs/customizations/20-ess.el
+++
@@ -1,9 +0,0 @@
-;;; ESS --- Summary
-;;; Commentary:
-;;; Code:
-
-;;; Loads ESS
-(require 'ess-site)
-;; (setq ess-nuke-trailing-whitespace-p t)
-(provide '20-ess)
-;;; 20-ess.el ends here
